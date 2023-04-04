883a853001f0612987adcbba57fba1223f558205
jwChou <129609717+jianweiChou@users.noreply.github.com>
2023-04-04 15:08:17 +0800

    Update index.js

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,4 +1,5 @@
 import wuiLimit from './wui-limit';
+import 'element-ui/lib/theme-chalk/index.css';
 const directives = [wuiLimit]
 const install = function (app) {
     directives.forEach((directive) => {
